372d3eb3f22bc05a74a7548b99eabeafce410b9b
calamus0427 <1211994507@qq.com>
2019-05-28 17:40:10 +0800

    error page

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,11 +2,11 @@ const pkg = require('./package')
 
 module.exports = {
   mode: 'spa',
-  router: {
-    base: '/',
+  // router: {
+  //   base: '/',
   //build时的路径设置
   // base: './',
-  },
+  // },
   /*
    ** Headers of the page
    */
